0245fb42ed1eacef0db9be8a59cc98a28ca12798
pmilne <philip@pmilne.net>
2021-04-16 19:31:27 +0100

    Tidy.

--- src/net/pmilne/GenerateSample.java
+++ src/net/pmilne/GenerateSample.java
@@ -16,15 +16,15 @@ public class GenerateSample {
 //                .map(x -> x)                              //      uniform distribution f(x) = 1
 //                .map(Math::sqrt)                          //       linear distribution f(x) = x
 //                .map(x -> pow(x, 1.0 / 3))                //    quadratic distribution f(x) = x^2
-//                .map(x -> pow(x, 0.25))                   //       cubic distribution  f(x) = x^3
-//                .map(x -> pow(x, 0.1))                    //       nonic distribution  f(x) = x^9
-//                .map(Math::log)                           // exponential distribution  f(x) = e^x
+//                .map(x -> pow(x, 0.25))                   //        cubic distribution f(x) = x^3
+//                .map(x -> pow(x, 0.1))                    //        nonic distribution f(x) = x^9
+//                .map(Math::log)                           //  exponential distribution f(x) = e^x
 //                .map(x -> x * x)
 //                .map(x -> x * sqrt(x))
 //                .map(x -> sqrt(-log(x)))
-//                .map(Erf::erfInv)                         //      normal distribution  f(x) = e^(-x^2)
-//                .map(x -> erfInv(centre(x)))              //      normal distribution mean 0
-                .map(x -> sqrt(2) * erfInv(centre(x)))    //      normal distribution mean = 0, std = 1
+//                .map(Erf::erfInv)                         //       normal distribution f(x) = e^(-x^2)
+//                .map(x -> erfInv(centre(x)))              //      normal distribution: mean 0
+                .map(x -> sqrt(2) * erfInv(centre(x)))    //      normal distribution: mean = 0, std = 1
                 .toArray();
         assert Arrays.stream(mapped).noneMatch(Double::isNaN);
         Sample sample = new Sample(100, mapped);
