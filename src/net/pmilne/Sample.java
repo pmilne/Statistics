560987a0515849d0521369756b5aff763458a955
pmilne <philip@pmilne.net>
2021-04-19 20:51:42 +0100

    Add support for single bucket, and dodge fencepost issue.

--- src/net/pmilne/Sample.java
+++ src/net/pmilne/Sample.java
@@ -6,32 +6,28 @@ import java.util.DoubleSummaryStatistics;
 import static java.lang.Math.floor;
 
 public class Sample {
-    public final int    size;
+    // Small constant to solve the fencepost issue and stop observation max falling off the end of the bucket list
+    private static final double SHRINK = (1 - 1e-10);
+
+    public final int size;
     public final double min;
     public final double max;
     public final double scale;
-    public final int[]  buckets;
+    public final int[] buckets;
 
     public Sample(int bucketCount, double[] observations) {
         DoubleSummaryStatistics stats = Arrays.stream(observations).summaryStatistics();
         this.size    = observations.length;
         this.min     = stats.getMin();
         this.max     = stats.getMax();
-        this.scale   = (max - min) / bucketCount;
+        this.scale   = (max - min) / (bucketCount * SHRINK);
         this.buckets = new int[bucketCount];
         // Place the observations in the appropriate buckets
         Arrays.stream(observations).forEach(observation -> buckets[getBucketFor(observation)]++);
     }
 
     private int getBucketFor(double observation) {
-        int result = (int) floor((observation - min) / scale);
-        // Special case the max observation and neighbours captured by rounding to solve the fencepost problem
-        // that arises when converting from the real range to the integer range. Effectively we are saying that
-        // observation max is equivalent to max - epsilon.
-        if (result == buckets.length) {
-            return result - 1;
-        }
-        return result;
+        return (int) floor((observation - min) / scale);
     }
 
     public double minObservationForBucket(int bucket) {
