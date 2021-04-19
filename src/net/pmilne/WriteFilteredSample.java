30ea657faef158a925da7d161505df198826e027
pmilne <philip@pmilne.net>
2021-04-19 21:01:30 +0100

    Tidy.

--- src/net/pmilne/WriteFilteredSample.java
+++ src/net/pmilne/WriteFilteredSample.java
@@ -7,8 +7,8 @@ import java.util.stream.DoubleStream;
 public class WriteFilteredSample {
     public static void main(String[] args) throws FileNotFoundException {
         PrintStream ps  = new PrintStream(new BufferedOutputStream(new FileOutputStream("Sample.csv")));
-        Random      gen = new Random(0);
-        DoubleStream.generate(gen::nextGaussian)
+        DoubleStream
+                .generate(new Random(0)::nextGaussian)
                 .map(x -> 1000000 + 1000 * x) // mean = 1000000, std = 1000
                 .mapToLong(Math::round)
                 .filter(l -> Long.bitCount(l) % 2 != 0)
